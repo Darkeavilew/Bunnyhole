2a4db9dd4a007afbbc605f26e4c3f52d011a9588
TrickTales <wolloyo@gmail.com>
2017-12-04 22:01:45 -0500

    Update server.js

--- chat-plugins/server.js
+++ chat-plugins/server.js
@@ -149,7 +149,7 @@ exports.commands = {
 		if (!target) return this.sendReply("/rmall [message] - Sends a pm to all users in the room.");
 		target = target.replace(/<(?:.|\n)*?>/gm, '');
 
-		let pmName = '~Spark Server';
+		let pmName = '~Bunnyhole Server';
 
 		for (let i in room.users) {
 			let message = '|pm|' + pmName + '|' + room.users[i].getIdentity() + '| ' + target;
