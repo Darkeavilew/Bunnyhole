243c537204d4696faf42c94e341022b64689da59
TrickTales <wolloyo@gmail.com>
2017-12-28 21:10:18 -0500

    Update passthebomb.js

--- chat-plugins/passthebomb.js
+++ chat-plugins/passthebomb.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function getUserName(id) {
+	return Users(id) ? Users(id).name : id;
+}
+
 class PassTheBomb extends Rooms.RoomGame {
 	constructor(room, seconds) {
 		super(room);
@@ -174,6 +178,7 @@ class PassTheBomb extends Rooms.RoomGame {
 	checkWinner() {
 		if (this.getSurvivors().length === 1) return true;
 	}
+	
 	getWinner() {
 		let winner = this.getSurvivors()[0][1].name;
 		let msg = '|html|<div class = "infobox"><center>The winner of this game of Pass the Bomb is <b style = "color:' + BH.nameColor(winner) + '">' + Chat.escapeHTML(winner) + '!</b> Congratulations!</center>';
