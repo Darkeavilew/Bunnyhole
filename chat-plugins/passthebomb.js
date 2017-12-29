8f92de84bc8fce340ad20b367932a4e90b5f66bf
TrickTales <wolloyo@gmail.com>
2017-12-28 20:22:39 -0500

    Update passthebomb.js

--- chat-plugins/passthebomb.js
+++ chat-plugins/passthebomb.js
@@ -127,14 +127,15 @@ class PassTheBomb extends Rooms.RoomGame {
 
 		this.madeMove = true;
 		this.setBomb(targetId);
-		this.room.add('|html|' + BH.nameColor(user.name) + ' passed the bomb to <b style = "' + BH.nameColor(targetId, true) + '">' + this.players.get(targetId).name + '</b>!');
+		this.room.add('|html|' + BH.nameColor(user.name) + ' passed the bomb to <strong style = "' + BH.nameColor(targetId, true) + '">' + this.players.get(targetId).name + '</strong>!');
+
 
 		if (this.checkWinner()) this.getWinner();
 	}
 	resetTimer() {
 		this.timer = setTimeout(() => {
 			let player = this.players.get(this.holder).name;
-			this.room.add('|html|<b>The bomb exploded and killed <span style = "' + BH.nameColor(this.holder, true) + '">' + Chat.escapeHTML(player) + '</span>').update();
+			this.room.add('|html|<b>The bomb exploded and killed <span style = "' + BH.nameColor(this.holder) + '">' + player + '</span>').update();
 			this.players.get(this.holder).status = 'dead';
 			this.canPass = false;
 			setTimeout(() => {
@@ -175,7 +176,7 @@ class PassTheBomb extends Rooms.RoomGame {
 	}
 	getWinner() {
 		let winner = this.getSurvivors()[0][1].name;
-		let msg = '|html|<div class = "infobox"><center>The winner of this game of Pass the Bomb is ' + BH.nameColor(winner) + Chat.escapeHTML(winner) + '! Congratulations!</center>';
+		let msg = '|html|<div class = "infobox"><center>The winner of this game of Pass the Bomb is <b style = "color:' + BH.nameColor(winner) + '">' + Chat.escapeHTML(winner) + '!</b> Congratulations!</center>';
 		this.room.add(msg).update();
 		this.end();
 	}
