57e431fd8a8f587d59392b93feba12cd7ffd80de
TrickTales <wolloyo@gmail.com>
2017-12-29 18:19:14 -0500

    Update badges.js

--- chat-plugins/badges.js
+++ chat-plugins/badges.js
@@ -85,7 +85,6 @@ exports.commands = {
 			selectedBadge = parts[1].trim();
 			if (!Db('badgeData').has(selectedBadge)) return this.errorReply("This badge does not exist, please check /badges list");
 			Db('badgeData').delete(selectedBadge);
-			let badgeUserObject = Db('userBadges').object();
 			let badgedUsers = Db('userBadges').keys().map(curUser => ({userid: curUser, badges: Db('userBadges').get(curUser)}));
 			badgedUsers.forEach(curUser => {
 				let badges = curUser.badges.filter(b => b !== selectedBadge);
