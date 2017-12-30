968ff366e2d1320b2db6e4501c5c5e33c776059c
TrickTales <wolloyo@gmail.com>
2017-12-30 12:07:38 -0500

    Update bh-uno.js

--- chat-plugins/bh-uno.js
+++ chat-plugins/bh-uno.js
@@ -603,7 +603,7 @@ exports.commands = {
 		new: function (target, room, user) {
 			if (!this.can("announce", null, room)) return false;
 			if (room.unoDisabled) return this.errorReply("UNO is currently disabled in this room.");
-			let cap = target ? parseInt(target, 10) : null;
+			let cap = target ? parseInt(target) : null;
 			if (room.game) return this.errorReply("There is already a game in progress in this room.");
 			if (cap && cap < 2) return this.errorReply("The player cap has to be at least 2.");
 
