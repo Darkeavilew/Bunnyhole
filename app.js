daf665913544d4d622a443de7b266eae2a55eaba
TrickTales <wolloyo@gmail.com>
2018-01-21 16:27:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -104,6 +104,8 @@ global.Punishments = require('./punishments');
 
 global.Chat = require('./chat');
 
+global.sqlite3 = require('sqlite3');
+
 global.Db = require('origindb')('config/db');
 
 global.BH = {};
