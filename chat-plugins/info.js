68cfa76c54a1cb2bfa9a4a55ab0a15bc3725a9f9
TrickTales <wolloyo@gmail.com>
2018-01-23 21:36:36 -0500

    Update info.js

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -274,11 +274,7 @@ exports.commands = {
 		if (!this.can('rangeban')) return;
 
 		let [ip, roomid] = this.splitOne(target);
-<<<<<<< HEAD
-		let targetRoom = target.length ? Rooms(target.trim()) : null;
-=======
 		let targetRoom = roomid ? Rooms(roomid) : null;
->>>>>>> 43c209155c0c1260c86826f16d56af9cd9c3cbda
 		if (!targetRoom && targetRoom !== null) return this.errorReply(`The room "${roomid}" does not exist.`);
 		let results = [];
 		let isAll = (cmd === 'ipsearchall');
